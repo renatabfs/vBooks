147a9e3211d12cb4bd79a7cc6acc74269619f97d
diozenio <d.dionisiobsl@gmail.com>
2022-08-20 18:00:40 -0300

    Pagina de cadastro

---
+++ api/lib/pages/cadastrar.dart
@@ -0,0 +1,123 @@
+import 'package:api/pages/login.dart';
+import 'package:api/widgets/form_input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Cadastrar extends StatefulWidget {
+  const Cadastrar({Key? key}) : super(key: key);
+
+  @override
+  State<Cadastrar> createState() => _CadastrarState();
+}
+
+class _CadastrarState extends State<Cadastrar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(32),
+        child: Container(
+          width: double.infinity,
+          constraints:
+              BoxConstraints(minHeight: MediaQuery.of(context).size.height),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SvgPicture.asset(
+                "assets/images/registrar_ilustracao.svg",
+                height: (MediaQuery.of(context).size.height) * 0.18,
+              ),
+              Form(
+                  autovalidateMode: AutovalidateMode.always,
+                  child: Column(
+                    children: [
+                      const FormInput(
+                          label: "Nome",
+                          hint: "Digite seu nome",
+                          obscure: false,
+                          suffixIcon: Icon(Icons.person_outline,
+                              color: Color(0xFFBDB8D9))),
+                      const FormInput(
+                          label: "E-mail",
+                          hint: "example@mail.com",
+                          obscure: false,
+                          suffixIcon: Icon(Icons.mail_outline_rounded,
+                              color: Color(0xFFBDB8D9))),
+                      const FormInput(
+                          hint: "Senha",
+                          label: "Senha",
+                          obscure: true,
+                          suffixIcon: Icon(Icons.lock_outline_rounded,
+                              color: Color(0xFFBDB8D9))),
+                    ],
+                  )),
+              Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF412F59),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: TextButton(
+                        onPressed: () {},
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            "Registrar-se",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xFFFFFFFF),
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        )),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) {
+                          return const Login();
+                        },
+                      ));
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Já possui uma conta?",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFFBDB8D9),
+                            fontFamily: "Poppins",
+                          ),
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text(
+                          "Entre",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF412F59),
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}

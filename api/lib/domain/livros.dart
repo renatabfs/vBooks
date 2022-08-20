a1b1633e04dd81b4201fa44632b22f1f37c82622
renatabfs <renata.niall66@gmail.com>
2022-08-20 18:52:51 -0300

    Livros adicionados ao BD

--- api/lib/domain/livros.dart
+++ api/lib/domain/livros.dart
@@ -3,12 +3,13 @@ class Livro {
   String titulo;
   String autor;
   String sinopse;
-
+  double id;
 Livro ({
   required this.imagem,
   required this.titulo,
   required this.autor,
-  required this.sinopse
+  required this.sinopse,
+  required this.id
 });
 }
 

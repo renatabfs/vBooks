4839b2a9c95cfa2b1eda4661d91f3901493a5c56
renatabfs <renata.niall66@gmail.com>
2022-08-20 17:59:03 -0300

    Banco de dados criado

---
+++ api/lib/domain/livros.dart
@@ -0,0 +1,14 @@
+class Livro {
+  String imagem;
+  String titulo;
+  String autor;
+  String sinopse;
+
+Livro ({
+  required this.imagem,
+  required this.titulo,
+  required this.autor,
+  required this.sinopse
+});
+}
+

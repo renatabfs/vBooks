f5df4e13af43e324a0bf97868f77f552ceabaf79
diozenio <d.dionisiobsl@gmail.com>
2022-08-21 16:34:10 -0300

    Sepração dos bancos de dados

---
+++ api/lib/data/autoresBD.dart
@@ -0,0 +1,29 @@
+import 'package:api/domain/autores.dart';
+
+class AutoresBD {
+  static List<Autores> listaAutores = [
+    Autores(
+      nome: "John D.",
+      imagem:
+          "https://media-exp1.licdn.com/dms/image/C4E03AQFkMw75gVBcsQ/profile-displayphoto-shrink_200_200/0/1516280999566?e=2147483647&v=beta&t=EHYQMy45LM0znBpFOb3WWqo3jAYokxu5-jhMHslv72E",
+      id: 1,
+    ),
+    Autores(
+      nome: "Robert C.M",
+      imagem: "https://m.media-amazon.com/images/I/61b7VTCUEfL._SY600_.jpg",
+      id: 2,
+    ),
+    Autores(
+      nome: "Pete G.",
+      imagem:
+          "https://images-na.ssl-images-amazon.com/images/I/41PFGXAhgUL._UY200_.png",
+      id: 3,
+    ),
+    Autores(
+      nome: "Douglas C.",
+      imagem:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Douglas_Crockford%2C_February_2013.jpg/1200px-Douglas_Crockford%2C_February_2013.jpg",
+      id: 4,
+    ),
+  ];
+}

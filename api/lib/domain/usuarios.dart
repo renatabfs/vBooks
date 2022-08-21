395b4f293adc90fd8de8eb552049686a1ce1fe85
diozenio <d.dionisiobsl@gmail.com>
2022-08-21 16:51:56 -0300

    Domain usuarios

---
+++ api/lib/domain/usuarios.dart
@@ -0,0 +1,12 @@
+class Usuarios {
+  int id;
+  String nome;
+  String email;
+  String senha;
+
+  Usuarios(
+      {required this.nome,
+      required this.email,
+      required this.senha,
+      required this.id});
+}

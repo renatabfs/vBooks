1c80f9844dbd2734af7d9fe0bcd1833d10598c2f
renatabfs <renata.niall66@gmail.com>
2022-08-21 19:50:36 -0300

    Branch de favoritos criada

--- api/lib/main.dart
+++ api/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:api/pages/favoritos.dart';
 import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:api/pages/landing_page.dart';
@@ -5,6 +6,6 @@ import 'package:api/pages/landing_page.dart';
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: LandingPage(),
+    home: Favoritos(),
   ));
 }

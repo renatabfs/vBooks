bfbcc1ff11027703c4860b193bc7d5e9ce7208c2
Necron090 <jcoc1@aluno.ifal.edu.br>
2022-08-28 18:52:02 -0300

    finalizada

--- api/lib/pages/home_page.dart
+++ api/lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:api/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:api/data/BD.dart';
 import 'package:api/domain/livros.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Home_page extends StatefulWidget {
   const Home_page({Key? key}) : super(key: key);
@@ -22,7 +23,7 @@ class _Home_pageState extends State<Home_page> {
         children: [
           Center(
             child: Padding(
-                padding: EdgeInsets.only(top: 0),
+                padding: EdgeInsets.only(top: 20),
                 child: SizedBox(
                     width: double.infinity,
                     height: 250,
@@ -34,7 +35,8 @@ class _Home_pageState extends State<Home_page> {
                             child: ClipRRect(
                               borderRadius: BorderRadius.circular(16),
                               child: Image.network(
-                                'https://www.ghibli.jp/gallery/baron004.jpg',
+                                'https://i.pinimg.com/736x/c3/b7/6d/c3b76d6bd59d6deeb3e5ddf105893654.jpg',
+                                fit: BoxFit.fill,
                                 width: 330,
                                 height: 200,
                               ),
@@ -47,17 +49,44 @@ class _Home_pageState extends State<Home_page> {
                                 width: 250,
                                 height: 115,
                                 decoration: BoxDecoration(
-                                  color: Color(0xFF4CAF50),
+                                  color: Color.fromARGB(255, 248, 247, 247),
                                   borderRadius: BorderRadius.circular(8),
                                 ),
-                                child: Text(
-                                  'oi',
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w600,
-                                    fontFamily: "Poppins",
-                                    color: Color(0x0FF412F59),
-                                  ),
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.all(8),
+                                      child: SizedBox(
+                                        child: Column(
+                                          children: [
+                                            Text(
+                                              'Olá, Fulano!',
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w600,
+                                                fontFamily: "Poppins",
+                                                color: Color(0x0FF412F59),
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 5,
+                                            ),
+                                            Text(
+                                              'Que tal escolher um livro para ler hoje?',
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.w600,
+                                                fontFamily: "Poppins",
+                                                color: Color(0x0FF9F8FBF),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 )),
                           )
                         ],

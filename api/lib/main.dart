d41143f5d2f565e3619a315cc1d7fdfb398ac2e4
Necron090 <jcoc1@aluno.ifal.edu.br>
2022-08-28 17:35:52 -0300

    cabeçalho em andamento

--- api/lib/main.dart
+++ api/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:api/pages/favoritos.dart';
+import 'package:api/pages/home_page.dart';
 import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:api/pages/landing_page.dart';
@@ -6,6 +6,6 @@ import 'package:api/pages/landing_page.dart';
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Favoritos(),
+    home: Home_page(),
   ));
 }
---
+++ api/lib/pages/home_page.dart
@@ -0,0 +1,90 @@
+import 'package:api/widgets/bookTemplate.dart';
+import 'package:api/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:api/data/BD.dart';
+import 'package:api/domain/livros.dart';
+
+class Home_page extends StatefulWidget {
+  const Home_page({Key? key}) : super(key: key);
+
+  @override
+  State<Home_page> createState() => _Home_pageState();
+}
+
+class _Home_pageState extends State<Home_page> {
+  List<Livro> lista = BD.listaLivros;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          Center(
+            child: Padding(
+                padding: EdgeInsets.only(top: 0),
+                child: SizedBox(
+                    width: double.infinity,
+                    height: 250,
+                    child: Center(
+                      child: Stack(
+                        children: <Widget>[
+                          Positioned(
+                            left: 15,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: Image.network(
+                                'https://www.ghibli.jp/gallery/baron004.jpg',
+                                width: 330,
+                                height: 200,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 55,
+                            top: 120,
+                            child: Container(
+                                width: 250,
+                                height: 115,
+                                decoration: BoxDecoration(
+                                  color: Color(0xFF4CAF50),
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Text(
+                                  'oi',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: "Poppins",
+                                    color: Color(0x0FF412F59),
+                                  ),
+                                )),
+                          )
+                        ],
+                      ),
+                    ))),
+          ),
+          grid(),
+        ],
+      ),
+    );
+  }
+
+  grid() {
+    return GridView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: 0.52,
+        //crossAxisSpacing: 2,
+      ),
+      itemCount: lista.length,
+      itemBuilder: (context, index) {
+        return BookTemplate(
+          livros: lista[index],
+        );
+      },
+    );
+  }
+}

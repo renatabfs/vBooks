7c261a3d5affb3ab827d1e6f75288a154f8ef430
renatabfs <renata.niall66@gmail.com>
2022-09-09 09:54:23 -0300

    Imagem search page corrigida

--- api/lib/pages/search_page.dart
+++ api/lib/pages/search_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
 
@@ -10,57 +9,58 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   @override
-
   Widget build(BuildContext context) {
-    return Scaffold( 
-      body: Column (
-        children: [
-           Container (
-            height: 150,
-        child: Padding (
-        padding: EdgeInsets.fromLTRB (36,40,36,48),
-        child: SizedBox (
-          width: 346,
-          height: 56,
-          child: TextField  (
-          style: TextStyle(
-            color: Color(0XFFBDB8D9)
-          ),
-          decoration: InputDecoration (
-          labelText: "Pesquisar livro",
-          filled: true,
-          fillColor: Color(0xFFEEEEEE),
-          prefixIcon: Icon(Icons.search),
-          enabledBorder: OutlineInputBorder (
-            borderSide: BorderSide(width: 0,
-            color: Colors.red,
-            style: BorderStyle.none,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(36, 15, 36, 0),
+                child: SizedBox(
+                  width: 346,
+                  height: 56,
+                  child: TextField(
+                    style: TextStyle(color: Color(0XFFBDB8D9)),
+                    decoration: InputDecoration(
+                      labelText: "Pesquisar livro",
+                      filled: true,
+                      fillColor: Color(0xFFEEEEEE),
+                      prefixIcon: Icon(Icons.search),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 0,
+                          color: Colors.red,
+                          style: BorderStyle.none,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            borderRadius: BorderRadius.circular(12),
-          ),
-          ),
-          ),
-          ),
+            Container(
+              width: 326,
+              height: 296,
+              child: Padding(
+                  padding: EdgeInsets.fromLTRB(54, 15, 54, 0),
+                  child: Container(
+                    width: 326,
+                    height: 296,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/searchimage.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )),
+            )
+          ],
         ),
       ),
-      Expanded (
-        child: Padding (
-          padding: EdgeInsets.fromLTRB(54, 168, 54, 270),
-        child: Container (
-          width: 326,
-          height: 296,
-          decoration: BoxDecoration (
-            image: DecorationImage (
-              image: AssetImage("assets/images/searchimage.png"),
-              alignment: Alignment.center,
-             
-          ),
-          ),
-          )
-      ),
-      )
-        ],
-      ),
-      );
+    );
   }
 }

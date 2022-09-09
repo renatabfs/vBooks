e740a3f03b9322a7f4830149b05a427d72273e72
renatabfs <renata.niall66@gmail.com>
2022-09-09 11:40:03 -0300

    Função de pesquisa adicionada

--- api/lib/pages/search_page.dart
+++ api/lib/pages/search_page.dart
@@ -1,3 +1,6 @@
+import 'package:api/data/livrosBD.dart';
+import 'package:api/domain/livros.dart';
+import 'package:api/widgets/bookTemplate.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatefulWidget {
@@ -8,59 +11,92 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  List<Livro> listaTotal = LivrosBD.listaLivros;
+  List<Livro> resultados = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 150,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(36, 15, 36, 0),
-                child: SizedBox(
-                  width: 346,
-                  height: 56,
-                  child: TextField(
-                    style: TextStyle(color: Color(0XFFBDB8D9)),
-                    decoration: InputDecoration(
-                      labelText: "Pesquisar livro",
-                      filled: true,
-                      fillColor: Color(0xFFEEEEEE),
-                      prefixIcon: Icon(Icons.search),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 0,
-                          color: Colors.red,
-                          style: BorderStyle.none,
-                        ),
-                        borderRadius: BorderRadius.circular(12),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(24, 40, 24, 40),
+          child: Column(
+            children: [
+              Form(
+                key: _formKey,
+                autovalidateMode: AutovalidateMode.always,
+                child: TextFormField(
+                  onChanged: (value) {
+                    resultados.clear();
+                    List<Livro> onChangedList = [];
+                    if (value != null && value.length > 0 && value != "") {
+                      for (var i = 0; i < listaTotal.length; i++) {
+                        if (listaTotal[i]
+                            .titulo
+                            .toLowerCase()
+                            .contains(value.toLowerCase())) {
+                          onChangedList.add(listaTotal[i]);
+                        }
+                      }
+                    }
+                    setState(() {
+                      resultados = onChangedList;
+                    });
+                  },
+                  style: TextStyle(color: Color(0XFFBDB8D9)),
+                  decoration: InputDecoration(
+                    labelText: "Pesquisar livro",
+                    filled: true,
+                    fillColor: Color(0xFFEEEEEE),
+                    prefixIcon: Icon(Icons.search),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 0,
+                        color: Colors.red,
+                        style: BorderStyle.none,
                       ),
+                      borderRadius: BorderRadius.circular(12),
                     ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              width: 326,
-              height: 296,
-              child: Padding(
-                  padding: EdgeInsets.fromLTRB(54, 15, 54, 0),
-                  child: Container(
-                    width: 326,
-                    height: 296,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/searchimage.png"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  )),
-            )
-          ],
+              buildListView(resultados),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  buildListView(lista) {
+    if (lista.length == 0) {
+      return Container(
+        height: 200,
+        margin: EdgeInsets.only(top: 150),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/searchimage.png"),
+            fit: BoxFit.contain,
+          ),
+        ),
+      );
+    } else {
+      return GridView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 0.52,
+          //crossAxisSpacing: 2,
+        ),
+        itemCount: lista.length,
+        itemBuilder: (context, index) {
+          return BookTemplate(
+            livro: lista[index],
+          );
+        },
+      );
+    }
   }
 }

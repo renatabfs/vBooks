8721c49236872c7ea13a977fc0e0f181cdb6d602
Dionísio Barbosa <68879723+diozenio@users.noreply.github.com>
2022-10-11 11:09:31 -0300

    Livros from json

--- api/lib/domain/livros.dart
+++ api/lib/domain/livros.dart
@@ -1,9 +1,9 @@
 class Livro {
-  String imagem;
-  String titulo;
-  String autor;
-  String sinopse;
-  int id;
+  late final String imagem;
+  late final String titulo;
+  late final String autor;
+  late final String sinopse;
+  late final int id;
 Livro ({
   required this.imagem,
   required this.titulo,
@@ -11,7 +11,24 @@ Livro ({
   required this.sinopse,
   required this.id
 });
+  
+// Livro.fromJson(Map<String, dynamic> json) {
+//     id = json['id'];
+//     imagem = json['imagem'];
+//     titulo = json['titulo'];
+//     autor = json['autor'];
+//     sinopse = json['sinopse'];
+//   }
+
+// Map<String, dynamic> toJson() {
+//     final Map<String, dynamic> data = new Map<String, dynamic>();
+//     data['id'] = this.id;
+//     data['imagem'] = this.imagem;
+//     data['titulo'] = this.titulo;
+//     data['autor'] = this.autor;
+//     data['sinopse'] = this.sinopse;
+//     return data;
+//   }
 
 
 }
-

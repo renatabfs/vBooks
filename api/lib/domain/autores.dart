2efdbcaca375c491323d21692457b84ebfa3ed07
evic08 <103127737+evic08@users.noreply.github.com>
2022-10-11 11:15:54 -0300

    Update autores.dart

--- api/lib/domain/autores.dart
+++ api/lib/domain/autores.dart
@@ -8,4 +8,19 @@ Autores ({
   required this.imagem,
   required this.id
   });
+  
+  
+  /*Autores.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    nome = json['nome'];
+    imagem = json['imagem'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['nome'] = this.nome;
+    data['imagem'] = this.imagem;
+    return data;
+  }*/
 }

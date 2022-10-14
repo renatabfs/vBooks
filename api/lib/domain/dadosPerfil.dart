893b27d08999a555c1e2c40482fab74706ed6e21
evic08 <evcc1@aluno.ifal.edu.br>
2022-10-14 13:10:15 -0300

    att 4

---
+++ api/lib/domain/dadosPerfil.dart
@@ -0,0 +1,12 @@
+import 'package:api/data/dadosPerfilBD.dart';
+import 'package:flutter/cupertino.dart';
+
+class DadosPerfil {
+  String dado;
+  Icon icon;
+
+  DadosPerfil({
+    required this.dado,
+    required this.icon,
+  });
+}
--- api/lib/pages/perfil.dart
+++ api/lib/pages/perfil.dart
@@ -1,51 +1,53 @@
 import 'package:api/controller/user_controller.dart';
-import 'package:api/domain/usuarios.dart';
-import 'package:api/pages/cadastrar.dart';
-import 'package:api/pages/navbar.dart';
-import 'package:api/widgets/form_input.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:api/data/usuariosBD.dart';
 import 'package:provider/provider.dart';
+import '../data/dadosPerfilBD.dart';
+import '../domain/dadosPerfil.dart';
+import '../widgets/perfilTemplate.dart';
 
 class Perfil extends StatefulWidget {
   const Perfil({Key? key}) : super(key: key);
+
+  String get dado => null;
+
 
   @override
   State <Perfil> createState() => _PerfilState();
 }
 
 class _PerfilState extends State <Perfil>{
-  List<Usuario> lista = UsuariosBD.listaUsuario;
+  List<DadosPerfil> lista = DadosPerfilBD.listaDados;
   @override
   Widget build(BuildContext context) {
     var usuarioProvider = Provider.of<UserController>(context);
 
     return Scaffold(
       backgroundColor: Color(0xFFFFFFFF),
-      body: Padding (
+      body: Padding(
         padding: EdgeInsets.all(32),
-         children SingleChildScrollView(
-      child: DottedBorder(
-      borderType: BorderType.RRect,
-          radius: Radius.circular(30),
-          //padding: EdgeInsets.all(6),
-          child: CircleAvatar(
-            radius: 28,
-            backgroundColor: Color(0x0FFB9D4D7),
-            child: const Text(
-              usuarioProvider.usuario.nome[0],
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-                fontFamily: "Poppins",
-                color: Colors.white,
-              ),
-            ),
-          )),
+        child: ListView(
+          children: [
+          //DottedBorder(
+           // borderType: BorderType.RRect,
+           // radius: Radius.circular(30),
+            //padding: EdgeInsets.all(6),
+            CircleAvatar(
+              radius: 28,
+              backgroundColor: Color(0x0FFB9D4D7),
+              child: Text(
+                usuarioProvider.usuario.nome[0],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: "Poppins",
+                  color: Colors.white,
+                    ),
+                  ),
+               ),
+             //),
         const SizedBox(height: 16),
-        child: const Text(
+        Text(
           "Editar dados do perfil",
           textAlign: TextAlign.center,
           style: TextStyle(
@@ -56,8 +58,22 @@ class _PerfilState extends State <Perfil>{
           ),
         ),
       const SizedBox(height: 16),
-      
+       buildListView(context),
+    ],
     ),
+         
+    );
+  }
+   buildListView(BuildContext context) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: lista.length,
+      itemBuilder: (context, index) {
+        return DadosPerfil(
+          listaDados: (lista[index].dado),
+        );
+      },
     );
   }
 }

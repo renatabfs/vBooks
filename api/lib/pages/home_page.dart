6d3e35e794d488034bc810d2311454d9803f7aa7
renatabfs <renata.niall66@gmail.com>
2022-10-27 10:47:12 -0300

    feat with future

--- api/lib/pages/home_page.dart
+++ api/lib/pages/home_page.dart
@@ -1,11 +1,10 @@
 import 'package:api/controller/user_controller.dart';
-import 'package:api/domain/usuarios.dart';
-import 'package:api/widgets/bookTemplate.dart';
 import 'package:api/widgets/grid.dart';
 import 'package:flutter/material.dart';
-import 'package:api/data/livrosBD.dart';
 import 'package:api/domain/livros.dart';
 import 'package:provider/provider.dart';
+
+import '../data/listaLivros.dart';
 
 class Home_page extends StatefulWidget {
   const Home_page({Key? key}) : super(key: key);
@@ -15,7 +14,7 @@ class Home_page extends StatefulWidget {
 }
 
 class _Home_pageState extends State<Home_page> {
-  List<Livro> lista = LivrosBD.listaLivros;
+  Future<List<Livro>> futureLista = LivrosDao().listarLivros();
 
   @override
   Widget build(BuildContext context) {
@@ -104,7 +103,7 @@ class _Home_pageState extends State<Home_page> {
           SizedBox(
             height: 32,
           ),
-          Grid(lista: lista),
+          Grid(futureLista: futureLista),
         ],
       ),
     );

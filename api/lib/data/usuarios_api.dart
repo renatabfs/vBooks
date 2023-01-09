990437b3685bc0cdf7b6b3a9d646b2b9f15d1f6e
diozenio <d.dionisiobsl@gmail.com>
2023-01-09 17:44:04 -0300

    fetch e add favorito

--- api/lib/data/usuarios_api.dart
+++ api/lib/data/usuarios_api.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
-import 'package:api/domain/livros.dart';
 import 'package:api/domain/usuarios.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
 class UsuariosApi {
-  final String _baseUrl = "localhost:8000";
+  final String _baseUrl = "192.168.2.129:8000";
 
   findUserById(String id) async {
     Uri url = Uri.http(_baseUrl, "/user/$id/info");
@@ -46,8 +45,24 @@ class UsuariosApi {
     return json;
   }
 
-  fetchFavorites() async {
-    Livro livro = Livro();
-    return [livro];
+  fetchFavorites(String userID) async {
+    Uri url = Uri.http(_baseUrl, "/user/$userID/favs");
+    Response response = await http.get(url);
+    var json = jsonDecode(response.body);
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load books');
+    }
+    return json;
+  }
+
+  addFavorite(String userID, String livroID) async {
+    Uri url = Uri.http(_baseUrl, "/favorite/new");
+    var body = jsonEncode({"livroId": livroID, "userId": userID});
+    Response response =
+        await http.post(url, body: body, headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    });
+    var json = jsonDecode(response.body);
+    return json;
   }
 }

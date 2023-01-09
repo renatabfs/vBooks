77b96404b2eb7f72b827e134fad1f6a129ecaa98
diozenio <d.dionisiobsl@gmail.com>
2023-01-09 17:42:49 -0300

    fetchFavoritos

--- api/lib/controller/user_controller.dart
+++ api/lib/controller/user_controller.dart
@@ -1,11 +1,28 @@
+import 'package:api/data/usuarios_api.dart';
+import 'package:api/domain/livros.dart';
 import 'package:api/domain/usuarios.dart';
 import 'package:flutter/material.dart';
 
 class UserController with ChangeNotifier {
   late Usuario _usuario;
+  late List<Livro> _favoritos = [];
   Usuario get usuario => this._usuario;
-  void setUsuario(Usuario user) {
+  List<Livro> get favoritos => this._favoritos;
+  void setUsuario(Usuario user) async {
     _usuario = user;
+    fetchFavoritos();
+    notifyListeners();
+  }
+
+  void fetchFavoritos() async {
+    var response = await UsuariosApi().fetchFavorites(_usuario.id);
+    for (var favorito in response) {
+      _favoritos.add(Livro.fromJson(favorito));
+    }
+    notifyListeners();
+  }
+
+  void handleFavorite(Livro livro) async {
     notifyListeners();
   }
 }
